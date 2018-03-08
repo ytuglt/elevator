be7ada5185f325e8fc736ab038e18467b5ae3770
gonglt1 <gonglt1@lenovo.com>
2018-03-08 09:41:32 +0800

    add splash activity

---
+++ app/src/main/java/com/shaoxia/elevator/SplashActivity.java
@@ -0,0 +1,18 @@
+package com.shaoxia.elevator;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by gonglt1 on 2018/3/7.
+ */
+
+public class SplashActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+
+}
